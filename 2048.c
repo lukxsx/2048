0c1e8dded0d2a583a948e3526a091eb2fde0f7df
Lukxsx <33071953+Lukxsx@users.noreply.github.com>
2021-11-13 15:52:41 +0200

    initial commit

---
+++ 2048.c
@@ -0,0 +1,100 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <termios.h>
+#include <unistd.h>
+
+int x_size = 4;
+int y_size = 4;
+
+void print_array(int** game_array) {
+	for (int j = 0; j < y_size; j++) {
+		for (int i = 0; i < x_size; i++) {
+			if (game_array[j][i] == 0) {
+				printf("   ");
+			} else {
+				printf(" %d ", game_array[j][i]);
+			}
+		}
+		printf("\n");
+	}
+}
+
+int is_empty(int** game_array, int x, int y) {
+	if (game_array[y][x] != 0) {
+		return 0;
+	}
+	else return 1;
+}
+
+int is_full(int** game_array) {
+	for (int j = 0; j < y_size; j++) {
+		for (int i = 0; i < x_size; i++) {
+			if (game_array[j][i] == 0) return 0;
+		}
+	}
+	return 1;
+}
+
+void create_random_tile(int** game_array, int value) {
+	int rx;
+	int ry;
+	while (1) {
+		srand(time(NULL));
+		rx = rand() % 4;
+		ry = rand() % 4;
+		if (is_empty(game_array, rx, ry)) {
+			break;
+		}
+	}
+	game_array[ry][rx] = value;
+}
+
+int** create_game_array() {
+	int** game_array;
+	game_array = malloc(y_size * sizeof(int *));
+    if (!game_array) {
+		exit(EXIT_FAILURE);
+	}
+	
+	for (int j = 0; j < y_size; j++) {
+		game_array[j] = malloc(x_size * sizeof(int));
+		if (!game_array[j]) {
+			for (int i = 0; i < j; i++) {
+				free(game_array[i]);
+			}
+			free(game_array);
+			exit(EXIT_FAILURE);
+		}
+		for (int i = 0; i < x_size; i++) {
+			game_array[j][i] = 0;
+		}
+	}
+	return game_array;
+}
+
+int main(void) {
+    static struct termios old_terminal, new_terminal;
+    tcgetattr(STDIN_FILENO, &old_terminal);
+    new_terminal = old_terminal;
+    new_terminal.c_lflag &= ~(ICANON | ECHO);          
+    tcsetattr(STDIN_FILENO, TCSANOW, &new_terminal);
+    
+    
+    int** game_array = create_game_array();
+    if (!game_array) {
+		return -1;
+	}
+
+    
+    
+    if (is_empty(game_array, 2, 3)) printf("joo\n");
+    create_random_tile(game_array, 2);
+    print_array(game_array);
+    for (int j = 0; j < y_size; j++) {
+		free(game_array[j]);
+	}
+    free(game_array);
+    tcsetattr(STDIN_FILENO, TCSANOW, &old_terminal);
+    return 0;
+}
